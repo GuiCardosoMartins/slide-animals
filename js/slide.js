6c893b6f89ca52648caa1c9c6c9c3ca233e603e0
Guilherme Cardoso <guicard.martins@gmail.com>
2022-01-19 20:15:33 -0300

    configuração index slide

--- js/slide.js
+++ js/slide.js
@@ -53,9 +53,39 @@ export default class Slide {
     this.onEnd = this.onEnd.bind(this);
   }
 
+  slidesPosition(slide) {
+    const margin = (this.wrapper.offsetWidth - slide.offsetWidth) / 2;
+    return -(slide.offsetLeft - margin);
+  }
+
+  slidesConfig() {
+    this.slideArray = [...this.slide.children].map((element) => {
+      const position = this.slidesPosition(element);
+      return { position, element };
+    });
+  }
+
+  slidesIndexNav(index) {
+    const last = this.slideArray.length - 1;
+    this.index = {
+      prev: index ? index - 1 : undefined,
+      active: index,
+      next: index === last ? undefined : index + 1
+    }
+  }
+
+  changeSlide(index) {
+    this.activeSlide = this.slideArray[index];
+    this.moveSlide(this.slideArray[index].position);
+    this.slidesIndexNav(index);
+    this.dist.finalPosition = this.activeSlide.position;
+  }
+
+
   init() {
     this.bindEvents();
     this.addSlideEvents();
+    this.slidesConfig();
     return this;
   }
 }
